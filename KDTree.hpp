105d292ae0abb899eb1e4a3a46f15c007262d0f5
Nguyễn Đức Hiển <88640081+DucHienIT@users.noreply.github.com>
2021-12-23 17:38:28 +0700

    Add files via upload

--- KDTree.hpp
+++ KDTree.hpp
@@ -1,10 +1,15 @@
 #pragma once
+#include <algorithm>
 #include <vector>
 #include <math.h>
 using namespace std;
 
 using point_t = vector<double>;
 using pointVec = vector<point_t>;
+using pointIndex = typename std::pair< std::vector< double >, size_t >;
+using pointIndexArr = typename std::vector< pointIndex >;
+
+#define DIM 3
 
 struct Node
 {
@@ -17,8 +22,22 @@ struct Node
         data = data_;
         level = level_;
     }
+    
+    
 };
 
+
+inline double dist2(const point_t a, const point_t b) {
+    double distc = 0;
+    for (size_t i = 0; i < DIM; i++) {
+        double di = a.at(i) - b.at(i);
+        distc += di * di;
+    }
+    return distc;
+}
+inline double dist(const point_t a, const point_t b) {
+    return sqrt(dist2(a, b));
+}
 double average(point_t a)
 {
     return a[0] + a[1] + a[2];
@@ -60,4 +79,145 @@ struct KDTree
         quickSort(data, 0, data.size()-1); 
         root = make_tree(data, 0, data.size()-1, 0);
     }
+    Node* nearestt(Node* branch, point_t pt, size_t level, Node* best, double best_dist)
+    {
+        
+        if ( (branch->left == nullptr && branch->right == nullptr)) {
+            return nullptr;
+        }
+
+        double d, dx, dx2;
+        point_t branch_pt = branch->data;
+        d = dist2(branch_pt, pt);
+        dx = branch_pt.at(level) - pt.at(level);
+        dx2 = dx * dx;
+
+        Node* best_l = best;
+        double best_dist_l = best_dist;
+
+        if (d < best_dist) 
+        {
+            best_dist_l = d;
+            best_l = branch;
+        }
+
+        size_t next_lv = (level + 1) % DIM;
+        Node* section;
+        Node* other;
+
+        if (dx > 0) 
+        {
+            section = branch->left;
+            other = branch->right;
+        }
+        else 
+        {
+            section = branch->right;
+            other = branch->left;
+        }
+
+        Node* further;
+        if (section != nullptr)
+        {
+            further = nearestt(section, pt, next_lv, best_l, best_dist_l);
+            if (further != nullptr)
+            {
+                double dl = dist2(further->data, pt);
+                if (dl < best_dist_l)
+                {
+                    best_dist_l = dl;
+                    best_l = further;
+                }
+            }
+
+        }
+        if (dx2 < best_dist_l && other != nullptr) 
+        {
+            further = nearestt(other, pt, next_lv, best_l, best_dist_l);
+            if (further != nullptr)
+            {
+                double dl = dist2(further->data, pt);
+                if (dl < best_dist_l) 
+                {
+                    best_dist_l = dl;
+                    best_l = further;
+                }
+            }
+            
+        }
+
+        
+        return best_l;
+    }
+    point_t nearest(point_t pt) 
+    {
+        size_t level = 0;
+        double branch_dist = dist2(root->data, pt);
+        Node* result = nearestt(root, pt, level, root,  branch_dist);
+        return result->data;
+    }
+
+    pointIndexArr neighborhood_(Node* branch, point_t pt, double rad, size_t level)
+    {
+        double d, dx, dx2;
+        if ( (branch->left == nullptr && branch->right == nullptr)) {
+            return pointIndexArr();
+        }
+
+        
+        double r2 = rad * rad;
+
+        point_t branch_pt = branch->data;
+        d = dist2(branch_pt, pt);
+        dx = branch_pt.at(level) - pt.at(level);
+        dx2 = dx * dx;
+
+
+        pointIndexArr nbh, nbh_s, nbh_o;
+        if (d <= r2) 
+        {
+            nbh.push_back(pointIndex(branch_pt, level));
+        }
+
+        Node* section;
+        Node* other;
+        if (dx > 0) {
+            section = branch->left;
+            other = branch->right;
+        }
+        else {
+            section = branch->right;
+            other = branch->left;
+        }
+        
+        if (section != nullptr)
+        {
+            nbh_s = neighborhood_(section, pt, rad, (level + 1) % DIM);
+            nbh.insert(nbh.end(), nbh_s.begin(), nbh_s.end());
+
+        }
+        if (dx2 < r2 && other != nullptr) 
+        {
+           nbh_o = neighborhood_(other, pt, rad, (level + 1) % DIM);
+            nbh.insert(nbh.end(), nbh_o.begin(), nbh_o.end());
+            
+        }
+        return nbh;
+        
+    }
+    pointIndexArr neighborhood(const point_t & pt, const double& rad) {
+        size_t level = 0;
+        return neighborhood_(root, pt, rad, level);
+    }
+    pointVec neighborhood_points(const point_t& pt, const double& rad) 
+    {
+        size_t level = 0;
+        pointIndexArr nbh = neighborhood_(root, pt, rad, level);
+        pointVec nbhp;
+        nbhp.resize(nbh.size());
+        std::transform(nbh.begin(), nbh.end(), nbhp.begin(), [](pointIndex x) { return x.first; });
+        return nbhp;
+    }
+    
+
 };
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void display(Node *p, int space)
 		if (p == nullptr) return ;
         space += 5;
         display(p->right, space);
-        for (int i = 5; i < space; i++) std::cout << " ";
+        for (int i = 5; i < space; i++) cout << " ";
         printf("(%d,%d,%d)\n", (int)p->data[0], (int)p->data[1], (int)p->data[2]);
         display(p->left, space);
 }
@@ -20,11 +20,30 @@ int main()
     for (int i = 9; i >= 0; i--)
     {
         point[0] = i;
+        point[1] = i + 1;
+        point[2] = i;
         data.push_back(point);
     }
     KDTree tree(data);
+
+    point_t p = {6,6,6};
+    point_t res = tree.nearest(p);
     
+    cout <<  res[0] << res[1] << res[2] << "\n\n";
+
+
     display(tree.root, 0);
-    
+
+    std::cout << "in tat ca neighborhood " ;
+
+
+    auto res2 = tree.neighborhood_points(p, 10);
+
+    for (point_t a : res2) {
+        for (double b : a) {
+            std::cout << b << " ";
+        }
+        std::cout << '\n';
+    }
     return 0;
 }
